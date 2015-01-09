983e56406b25854d3625f26e08723b68e7df80ff
Martinus Ady H <martinusadyh@gmail.com>
2015-01-10 01:51:03 +0700

    Update script untuk tambah opsi posting

--- helper.sh
+++ helper.sh
@@ -3,11 +3,37 @@
 # Helper script untuk posting pake octopress, karena ga hafal klo harus
 # jalanin manual :'(
 
+judul=""
+file=""
+
 new_post() {
     echo -n "Judul postingan: "
     read judul
 
-    rake new_post["$judul"]
+    echo -n "Masukkan kategori: "
+    read kategori
+
+    tmp=`rake new_post["$judul"]`
+
+    # ambil direktori source/post + trim string-nya :)
+    # klo di java mah sama kek split(":")[1].trim()
+    file=`echo $tmp | cut -d ':' -f 2 | tr -d ' '`
+
+    echo "Berhasil membuat $file"
+
+    # Tambah opsi sharing true
+    sharingline='sharing: true'
+    commentline='comments: true'
+    sed -i "s|$commentline|$commentline\n$sharingline|" $file
+
+    categories='categories:'
+    kategori="- $kategori"
+    sed -i "s|$categories|$categories\n$kategori|" $file
+
+    tags_label="tags:"
+
+    # insert tags
+    sed -i "s|$kategori|$kategori\n$tags_label|" $file
 }
 
 publish() {
